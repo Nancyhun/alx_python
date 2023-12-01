2a3e080be5b2d950823ddf2c0beb50d9c0bc3880
Nancy <nancyoforiaddo17@gmail.com>
2023-12-01 00:13:45 +0000

    Python Project 2

---
+++ python-functions/0-sum.py
@@ -0,0 +1,16 @@
+def add(a, b):
+    """
+    Adds two integers and returns the result.
+    
+    param a: The first integer.
+    param b: The second integer.
+    
+    return: The sum of a and b.
+    """
+    return a + b
+
+print(add(1, 2))
+print(add(98, 0))
+print(add(100, -2))
+
+
--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
 import random
 
-number = random.randint(-10, 10)
+# num = [random.randint(-10,10) for i in range(10)]
 
+# for x in num:
+#     if x > 0:
+#         print(x,"is positive")
+#     elif x < 0:
+#         print(x,'is negative')
+#     else:
+#         print(x,'is zero')
+
+
+number = random.randint(-10, 10)
 if number > 0:
     print(number,"is positive")
 
